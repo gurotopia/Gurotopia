4fc72891207ee33e3319cc9cc13d48f6f1c95609
leeendl <127639272+leeendl@users.noreply.github.com>
2025-06-20 16:11:58 -0500

    you cannot trash/drop your fist or wrench

--- include/action/drop.cpp
+++ include/action/drop.cpp
@@ -6,20 +6,32 @@
 
 void drop(ENetEvent event, const std::string& header)
 {
-    std::string id{readch(std::move(header), '|')[4]};
-    if (id.empty()) return;
+    std::string itemID = readch(std::move(header), '|')[4];
+
+    if (itemID.empty()) return;
+    short id = stoi(itemID);
+
+    if (id == 32 || id == 18) // @todo add untradable items too
+    {
+        gt_packet(*event.peer, false, 0, { "OnTextOverlay", "You can't drop that." });
+        return;
+    }
     
     for (const slot &slot : _peer[event.peer]->slots)
-        if (slot.id == stoi(id)) {
+        if (slot.id == id) 
+        {
             gt_packet(*event.peer, false, 0, {
                 "OnDialogRequest", 
-                std::format("set_default_color|`o\n"
-                "add_label_with_icon|big|`wDrop {0}``|left|{1}|\n"
-                "add_textbox|How many to drop?|left|\n"
-                "add_text_input|count||{2}|5|\n"
-                "embed_data|itemID|{1}\n"
-                "end_dialog|drop_item|Cancel|OK|\n", items[stoull(id)].raw_name, id, slot.count).c_str()
+                std::format(
+                    "set_default_color|`o\n"
+                    "add_label_with_icon|big|`wDrop {0}``|left|{1}|\n"
+                    "add_textbox|How many to drop?|left|\n"
+                    "add_text_input|count||{2}|5|\n"
+                    "embed_data|itemID|{1}\n"
+                    "end_dialog|drop_item|Cancel|OK|\n", 
+                    items[id].raw_name, id, slot.count
+                ).c_str()
             });
-            break; // early exit
+            return;
         }
 }
--- include/action/trash.cpp
+++ include/action/trash.cpp
@@ -6,24 +6,32 @@
 
 void trash(ENetEvent event, const std::string& header)
 {
-    std::string id = readch(std::move(header), '|')[4];
-    if (id.empty()) return;
+    std::string itemID = readch(std::move(header), '|')[4];
+
+    if (itemID.empty()) return;
+    short id = stoi(itemID);
+    
+    if (id == 32 || id == 18) // @todo add untradable items too
+    {
+        gt_packet(*event.peer, false, 0, { "OnTextOverlay", "You'd be sorry if you lost that!" });
+        return;
+    }
 
     for (const slot &slot : _peer[event.peer]->slots)
-        if (slot.id == stoi(id))
+        if (slot.id == id)
         {
-            item &item = items[slot.id];
-            if (item.type == std::byte{ type::WRENCH } || item.type == std::byte{ type::FIST }) return;
             gt_packet(*event.peer, false, 0, {
                 "OnDialogRequest",
-                std::format("set_default_color|`o\n"
-                "add_label_with_icon|big|`4Recycle`` `w{0}``|left|{1}|\n"
-                "add_label|small|You will get 0 gems per item.|left|\n" // @todo get rgt gem amount for trashing
-                "add_textbox|How many to `4destroy``? (you have {2})|left|\n"
-                "add_text_input|count||0|5|\n"
-                "embed_data|itemID|{1}\n"
-                "end_dialog|trash_item|Cancel|OK|\n",
-                item.raw_name, slot.id, slot.count).c_str()
+                std::format(
+                    "set_default_color|`o\n"
+                    "add_label_with_icon|big|`4Recycle`` `w{0}``|left|{1}|\n"
+                    "add_label|small|You will get 0 gems per item.|left|\n" // @todo get rgt gem amount for trashing
+                    "add_textbox|How many to `4destroy``? (you have {2})|left|\n"
+                    "add_text_input|count||0|5|\n"
+                    "embed_data|itemID|{1}\n"
+                    "end_dialog|trash_item|Cancel|OK|\n",
+                    items[slot.id].raw_name, slot.id, slot.count
+                ).c_str()
             });
             return;
         }

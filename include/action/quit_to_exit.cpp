5714506d3d5fb44f591eb5bc2423e1b60ac07454
leeendl <127639272+leeendl@users.noreply.github.com>
2025-06-30 15:19:36 -0500

    default netid is 0 instead of -1 (like in growtopia)

--- include/action/quit_to_exit.cpp
+++ include/action/quit_to_exit.cpp
@@ -21,6 +21,6 @@ void action::quit_to_exit(ENetEvent& event, const std::string& header, bool skip
     });
     if (it->second.visitors <= 0) worlds.erase(it);
     if (prefix == "2" || prefix == "c") prefix = "w";
-    peer->netid = -1; // this will fix any packets being sent outside of world
+    peer->netid = 0; // this will fix any packets being sent outside of world
     if (!skip_selection) on::RequestWorldSelectMenu(event);
 }
--- include/database/peer.hpp
+++ include/database/peer.hpp
@@ -36,7 +36,7 @@
     public:
         peer& read(const std::string& name);
 
-        signed netid{ -1 }; // @note peer's netid is world identity. this will be useful for many packet sending
+        signed netid{ 0 }; // @note peer's netid is world identity. this will be useful for many packet sending
         int user_id{}; // @note unqiue user id.
         std::array<std::string, 2zu> ltoken{}; // @note peer's ltoken e.g. {growid, password}
         std::string prefix{ "w" }; // @note display name color, default: "w" (White)

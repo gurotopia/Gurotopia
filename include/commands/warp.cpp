f6500207408dac8ddd1ecc8e43b365272c39ad25
Purri0nCS <LvAleksandrsLv@gmail.com>
2025-07-27 00:01:50 +0300

    some checks, for warp and weather.

--- include/commands/warp.cpp
+++ include/commands/warp.cpp
@@ -5,8 +5,15 @@
 
 void warp(ENetEvent& event, const std::string_view text)
 {
-    std::string world_name{ text.substr(sizeof("warp ")-1) };
-    std::for_each(world_name.begin(), world_name.end(), [](char& c) { c = std::toupper(c); });
+    if (text.length() <= sizeof("warp ") - 1) {
+        packet::create(*event.peer, false, 0, {
+            "OnConsoleMessage",
+            "`^Usage: /warp <world name>"
+        });
+        return;
+    }
+
+    std::string world_name{ text.substr(sizeof("warp ") - 1) };
 
     packet::action(*event.peer, "log", std::format("msg| `6/warp {}``", world_name));
     packet::create(*event.peer, false, 0, { "OnSetFreezeState", 1 });

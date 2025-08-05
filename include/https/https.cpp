e9342f36e828716c40a87e9741fd4344b666f482
leeendl <127639272+leeendl@users.noreply.github.com>
2025-08-05 18:23:23 -0500

    improve https security

--- include/https/https.cpp
+++ include/https/https.cpp
@@ -22,8 +22,6 @@
 #endif
 using namespace std::literals::chrono_literals;
 
-std::unordered_map<std::string, https::client> https::clients{};
-
 void https::listener(_server_data server_data)
 {
     OpenSSL_add_all_algorithms();
@@ -36,6 +34,11 @@ void https::listener(_server_data server_data)
     if (SSL_CTX_use_certificate_file(ctx, "resources/ctx/server.crt", SSL_FILETYPE_PEM) <= 0 ||
         SSL_CTX_use_PrivateKey_file(ctx, "resources/ctx/server.key", SSL_FILETYPE_PEM) <= 0)
             ERR_print_errors_fp(stderr);
+
+    SSL_CTX_set_options(ctx, SSL_OP_NO_SSLv2 | SSL_OP_NO_SSLv3 | SSL_OP_NO_COMPRESSION);
+    
+    SSL_CTX_set_cipher_list(ctx, "HIGH:!aNULL:!MD5:!RC4:!3DES");
+    SSL_CTX_set_ecdh_auto(ctx, 1);
 
     SOCKET socket = ::socket(AF_INET, SOCK_STREAM, 0);
     struct sockaddr_in addr;
@@ -69,37 +72,38 @@ void https::listener(_server_data server_data)
             "\r\n{}",
             Content.size(), Content).c_str();
 
-    listen(socket, 18);
+#ifndef _WIN32
+    setsockopt(socket, IPPROTO_TCP, TCP_DEFER_ACCEPT, &enable, sizeof(enable));
+#endif
+
+    listen(socket, 9);
     while (true)
     {
         SOCKET fd = accept(socket, (struct sockaddr*)&addr, &addrlen);
         if (fd < 0) continue;
 
-        char ip[INET_ADDRSTRLEN]; // @note allocated LPSTR 
-        inet_ntop(AF_INET, &addr.sin_addr, ip, INET_ADDRSTRLEN);
+        SSL *ssl = SSL_new(ctx);
+        SSL_set_fd(ssl, fd);
 
-        https::client &client = clients[std::string{ip}];
+        struct timeval timeout{.tv_sec = 3};
+        setsockopt(fd, SOL_SOCKET, SO_RCVTIMEO, (char*)&timeout, sizeof(timeout));
+        setsockopt(fd, SOL_SOCKET, SO_SNDTIMEO, (char*)&timeout, sizeof(timeout));
 
-        if (steady_clock::now() - client.last_connect >= 3s)
+        if (SSL_accept(ssl) > 0)
         {
-            SSL *ssl = SSL_new(ctx);
-            SSL_set_fd(ssl, fd);
-            if (SSL_accept(ssl) > 0)
-            {
-                char buf[600/*213*/]; // @note size of growtopia's request.
-                int length{ sizeof(buf ) }; // @note openssl is written in C so this will act as std::string::length()
+            char buf[213]; // @note size of growtopia's POST request.
+            int length{ sizeof(buf ) };
 
-                if (SSL_read(ssl, buf, length) > 0)
-                {
-                    printf("%s", buf);
-                    if (std::string_view{buf, sizeof(buf )}.contains("POST /growtopia/server_data.php HTTP/1.1"))
-                        SSL_write(ssl, response.c_str(), response.size());
-                }
-                else ERR_print_errors_fp(stderr);
+            if (SSL_read(ssl, buf, length) == length)
+            {
+                printf("%s", buf);
+                if (std::string_view{buf, sizeof(buf )}.contains("POST /growtopia/server_data.php HTTP/1.1"))
+                    SSL_write(ssl, response.c_str(), response.size());
             }
-            else ERR_print_errors_fp(stderr);
-            SSL_free(ssl);
+            else ERR_print_errors_fp(stderr); // @note we don't accept growtopia GET. this error is normal if appears.
         }
+        else ERR_print_errors_fp(stderr);
+        SSL_free(ssl);
 #ifdef _WIN32
             closesocket(fd);
 #else
